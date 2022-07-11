a0a8638fb23d593ba6cf161b40b4422cc5009858
sakyamous <sakyamous@gmail.com>
2022-07-12 03:56:10 +0700

    upload file

---
+++ README.md
@@ -0,0 +1,4 @@
+# StudentList
+CRUD Student Data using SQLite and Kotlin
+
+
---
+++ app/src/main/java/com/example/studentlist/Student.kt
@@ -0,0 +1,11 @@
+package com.example.studentlist
+
+/**
+ * Created by Nadya Aditama on 11 Desember 2019
+ */
+
+data class Student (
+    var nim : Int = 0,
+    var name : String = "",
+    var faculty : String = ""
+)
